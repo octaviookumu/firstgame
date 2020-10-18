058f149e1dc0f98bd7d8dc5cb9506634bdf714db
octavianhokela <octavian@hokela.co.ke>
2020-10-18 21:34:50 +0300

    Printing name and adding some inputs

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Yeah")
+print("Welcome to the game of your life!!")
+name = input("What is your name? ")
+age = input("What is your age? ")
+print(f"{name} {age}")
+
+is_older = int(age) >= 18
+print(is_older)
